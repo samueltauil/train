31303e6201e558bb030aa92f52cde5591c46f5d2
Samuel Tauil <samueltauil@gmail.com>
2012-10-07 22:47:15 -0300

    exercise 7

--- src/main/java/br/train/graph/Graph.java
+++ src/main/java/br/train/graph/Graph.java
@@ -167,4 +167,23 @@ public class Graph {
         return String.valueOf(solution);
     }
 
+
+
+    //TODO to be analyzed and maybe used as new version of DFS
+
+//    public void DFS() {
+//        VertexState state[] = new VertexState[vertexCount];
+//        for (int i = 0; i < vertexCount; i++)
+//            state[i] = VertexState.White;
+//        runDFS(0, state);
+//    }
+
+//    public void runDFS(int u, VertexState[] state) {
+//        state[u] = VertexState.Gray;
+//        for (int v = 0; v < vertexCount; v++)
+//            if (isEdge(u, v) && state[v] == VertexState.White)
+//                runDFS(v, state);
+//        state[u] = VertexState.Black;
+//    }
+
 }
